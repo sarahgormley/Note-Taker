f4fd8cea074b7a65b501d8f89a8e979d634f6548
SarahGormley <sarah.gormley39@gmail.com>
2022-05-26 17:16:43 +1000

    get request to get notes from dbjson file done

--- Routes/notes.js
+++ Routes/notes.js
@@ -1,11 +1,29 @@
 // Require express notes router
+const notesRoute = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
 
 // EMpty array to store notes
+let notesArr = [];
 
-// Get request
+// Get request to get notes from db dson
+notesRoute.get('/', (req, res) => {
+    fs.readFile('./db/db.json', (error, data) => {
+        if (error) {
+            console.error(error)
+        } else {
+            res.json(JSON.parse(notesArr))
+        }
+    })
+})
 
 //Post request
+notesRoute.post('/', (req, res) => {
+
+})
 
 //Delete
 
 //Write database to write ntoes and write a json respo
+module.exports = notes;
